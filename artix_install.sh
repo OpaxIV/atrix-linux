2984c2ac1eacc84ae67c4deb3b544872166cf0c0
OpaxIV <93701325+OpaxIV@users.noreply.github.com>
2023-11-01 20:31:44 +0100

    Update artix_install.sh

--- artix_install.sh
+++ artix_install.sh
@@ -9,10 +9,11 @@
 
 # 2do:
 #	- add wlan support (currently only working with ethernet)
+#	- choose init system (might only work on arch, not artix)
+#	- choose kernel to install (might only work on arch, not artix)
 
 
 # References:
-# This script has been created with the help of the following URLs:
 #	- https://www.youtube.com/watch?v=FR3czoXpPoc
 #	- https://github.com/Zaechus/artix-installer/blob/main/install.sh
 #	- https://github.com/Zaechus/artix-installer/blob/main/src/installer.sh
@@ -35,27 +36,103 @@ loadkeys de_CH-latin1
 
 
 
-## Check Boot Mode
-
+### Check Boot Mode ###
 [ ! -d /sys/firmware/efi ] && printf ""
-
 
 if [ ! -d /sys/firmware/efi ]; then
 	printf "BIOS System detected"
 	
 else
 	prinf "UEFI System detected"
-	
+fi	
 
-## Install NetworkManager (on hold, can be ignored with ethernet)
+
+
+
+
+
+
+### Install NetworkManager (on hold, can be ignored with ethernet)###
 #print "Installing and enabling NetworkManager."
-pacstrap /mnt networkmanager >/dev/null
+#pacstrap /mnt networkmanager >/dev/null
 
 #systemctl enable NetworkManager --root=/mnt &>/dev/null
 
-	
-	
-## Format Drive(s) and Create Partitions
+
+
+
+
+
+
+
+
+
+
+
+
+### Setting Passwords for User and LUKS Container ###
+
+## Set Password for User Account
+print "Please set a password for the User Account."
+
+# read from the file descriptor, -s = disable echo, -r = disable backslashes
+read -r -s userpassword
+
+# Check user input
+if [[ -z "$userpassword" ]]; then													## -z = true if empty string or an uninitialized variable
+        echo
+        error_print "No user password input, please try again."
+		
+fi
+
+print "Repeat password."
+read -r -s userpassword2
+
+echo
+if [[ "$userpassword" != "$userpassword2" ]]; then
+	error_print "User Passwords not matching, please try again."
+fi
+
+
+
+
+
+## Set Password for the LUKS Container. --> saved into variable for later usage
+print "Please set a password for the LUKS encrypted partition."
+
+# read from the file descriptor, -s = disable echo, -r = disable backslashes
+read -r -s lukspassword
+
+# Check user input
+if [[ -z "$lukspassword" ]]; then													## -z = true if empty string or an uninitialized variable
+        echo
+        error_print "No luks password input, please try again."
+		
+fi
+
+print "Repeat password."
+read -r -s lukspassword2
+
+echo
+if [[ "$lukspassword" != "$lukspassword2" ]]; then
+	error_print "Passwords not matching, please try again."
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Format Drive(s) and Create Partitions ###
 
 # Choosing disk for the installation.
 print "Available disks for the installation:"
@@ -68,22 +145,26 @@ do
 done
 
 
-## Set Password for the LUKS Container. --> saved into variable for later usage
-print "Please set a password for the LUKS encrypted partition."
 
-# read from the file descriptor, -s = disable echo, -r = disable backslashes
-read -r -s password
 
-# Check user input
-if [[ -z "$password" ]]; then													## -z = true if empty string or an uninitialized variable
-        echo
-        error_print "No password input, please try again."
-		
-## fi?
 
-print "Repeat password."
-read -r -s password2
 
-echo
-if [[ "$password" != "$password2" ]]; then
-	error_print "Passwords not matching, please try again."
+
+
+
+
+
+
+
+## Partitioning
+
+# Create Boot Partition
+# Format as FAT32
+
+
+# Create Encrypted LUKS Home Partition
+# Format as LUKS
+
+
+
+## Mount the Partitions
